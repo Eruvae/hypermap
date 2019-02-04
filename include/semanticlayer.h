1e56cdabd16929565ae3742f50f57927e8ff3908
Tobias Zaenker <tobias.zaenker@aalto.fi>
2019-02-04 20:27:09 +0200

    Test for combined keyword+region query in semantic layer, added test service to map server

--- include/semanticlayer.h
+++ include/semanticlayer.h
@@ -3,10 +3,14 @@
 
 #include "ros/ros.h"
 #include "maplayerbase.h"
+#include "geometry_msgs/Point.h"
+#include "geometry_msgs/Polygon.h"
 #include <boost/geometry.hpp>
 #include <boost/geometry/index/rtree.hpp>
 #include <vector>
 #include <string>
+#include <set>
+#include <map>
 
 namespace bg = boost::geometry;
 namespace bgi = boost::geometry::index;
@@ -26,8 +30,43 @@ class SemanticLayer : public MapLayerBase
     polygon shape;
   };
 
-  bgi::rtree< value, bgi::rstar<16> > rtree;
+  bgi::rtree< value, bgi::rstar<16> > objectRtree;
   std::vector<SemanticObject> objectList;
+  std::map<std::string, std::set<size_t>> objectMap;
+
+  geometry_msgs::Point boostToPointMsg(const point &p)
+  {
+      geometry_msgs::Point pm;
+      pm.x = p.get<0>();
+      pm.y = p.get<1>();
+      return pm;
+  }
+
+  geometry_msgs::Point32 boostToPoint32Msg(const point &p)
+  {
+      geometry_msgs::Point32 pm;
+      pm.x = p.get<0>();
+      pm.y = p.get<1>();
+      return pm;
+  }
+
+  point pointMsgToBoost(const geometry_msgs::Point &pm)
+  {
+      return point(pm.x, pm.y);
+  }
+
+  point point32MsgToBoost(const geometry_msgs::Point32 &pm)
+  {
+      return point(pm.x, pm.y);
+  }
+
+  polygon polygonMsgToBoost(const geometry_msgs::Polygon &pgm)
+  {
+      polygon pg;
+      for (const auto &p : pgm.points)
+        bg::append(pg.outer(), point32MsgToBoost(p));
+      return pg;
+  }
 
 public:
   SemanticLayer() : MapLayerBase("map") {}
@@ -35,24 +74,66 @@ public:
   int getIntValue(double xPos, double yPos);
   std::string getStringValue(double xPos, double yPos);
 
-  std::vector<std::string> getObjectsInRange(double xmin, double ymin, double xmax, double ymax)
+  std::set<size_t> getObjectsInRange(double xmin, double ymin, double xmax, double ymax)
   {
       return getObjectsInRange(point(xmin, ymin), point(xmax, ymax));
   }
 
-  std::vector<std::string> getObjectsInRange(const point &minCorner, const point &maxCorner)
+  std::set<size_t> getObjectsInRange(const point &minCorner, const point &maxCorner)
   {
       box query_box(minCorner, maxCorner);
       std::vector<value> result_obj;
-      std::vector<std::string> result;
-      rtree.query(bgi::intersects(query_box), std::back_inserter(result_obj));
+      std::set<size_t> result;
+      objectRtree.query(bgi::intersects(query_box), std::back_inserter(result_obj));
       for (value val : result_obj)
       {
           SemanticObject foundObject = objectList[val.second];
           if (bg::intersects(query_box, foundObject.shape))
-              result.push_back(foundObject.name);
+              result.insert(val.second);
       }
       return result;
+  }
+
+  std::set<size_t> getObjectsInRange(const polygon &pg)
+  {
+      std::vector<value> result_obj;
+      std::set<size_t> result;
+      objectRtree.query(bgi::intersects(pg), std::back_inserter(result_obj));
+      for (value val : result_obj)
+      {
+          SemanticObject foundObject = objectList[val.second];
+          if (bg::intersects(pg, foundObject.shape))
+              result.insert(val.second);
+      }
+      return result;
+  }
+
+  std::set<size_t> getObjectsByName(const std::string &name)
+  {
+      return objectMap[name];
+  }
+
+  std::set<size_t> getObjectsByNameInRange(const std::string &name, const point &minCorner, const point &maxCorner)
+  {
+      auto nameSet = getObjectsByName(name);
+      auto rangeSet = getObjectsInRange(minCorner, maxCorner);
+      std::set<size_t> intersection;
+      std::set_intersection(nameSet.begin(), nameSet.end(), rangeSet.begin(), rangeSet.end(), std::inserter(intersection, intersection.end()));
+      return intersection;
+  }
+
+  std::vector<std::pair<geometry_msgs::Point, std::string>> getStringReps(const geometry_msgs::Polygon &area)
+  {
+      std::set<size_t> qres = getObjectsInRange(polygonMsgToBoost(area));
+      std::vector<std::pair<geometry_msgs::Point, std::string>> res;
+      for (size_t i : qres)
+      {
+          const SemanticObject &obj = objectList[i];
+          point center;
+          bg::centroid(obj.shape, center);
+          res.push_back(std::make_pair(boostToPointMsg(center), obj.name));
+      }
+      return res;
   }
 
   void addExampleObject()
@@ -64,7 +145,8 @@ public:
       box bb = bg::return_envelope<box>(testPoly);
       SemanticObject obj = {"TestTriangle", testPoly};
       objectList.push_back(obj);
-      rtree.insert(std::make_pair(bb, objectList.size() - 1));
+      objectRtree.insert(std::make_pair(bb, objectList.size() - 1));
+      objectMap[obj.name].insert(objectList.size() - 1);
   }
 
   void printQuery()
@@ -75,7 +157,7 @@ public:
       bg::append(testPoly.outer(), point(0.0, 1.0));
       bg::append(testPoly.outer(), point(1.0, 0.0));
       std::vector<value> result_s;
-      rtree.query(bgi::intersects(query_box), std::back_inserter(result_s));
+      objectRtree.query(bgi::intersects(query_box), std::back_inserter(result_s));
       ROS_INFO("Printing found objects");
       for (value val : result_s)
       {
--- src/map_server.cpp
+++ src/map_server.cpp
@@ -2,6 +2,14 @@
 #include "maplayerbase.h"
 #include "semanticlayer.h"
 
+#include "hypermap/HypermapImage.h"
+#include "geometry_msgs/Point.h"
+#include "geometry_msgs/Polygon.h"
+
+#include "hypermap/RetrieveStrVals.h"
+
+SemanticLayer layer;
+
 //#include "beginner_tutorials/AddTwoInts.h"
 
 /*bool add(beginner_tutorials::AddTwoInts::Request  &req,
@@ -13,6 +21,17 @@
   return true;
 }*/
 
+bool retrieveStrVals(hypermap::RetrieveStrVals::Request &req, hypermap::RetrieveStrVals::Response &res)
+{
+    const auto &qres = layer.getStringReps(req.area);
+    for (const auto &obj : qres)
+    {
+        res.locations.push_back(obj.first);
+        res.names.push_back(obj.second);
+    }
+    return true;
+}
+
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "map_server");
@@ -20,10 +39,12 @@ int main(int argc, char **argv)
 
   ROS_INFO("Map server initialized");
 
-  SemanticLayer layer;
+  geometry_msgs::Point p;
+
   layer.addExampleObject();
   layer.printQuery();
 
+  ros::ServiceServer service = nh.advertiseService("retrieve_string_vals", retrieveStrVals);
   //ros::ServiceServer service = nh.advertiseService("add_two_ints", add);
   //ROS_INFO("Ready to add two ints.");
   ros::spin();
